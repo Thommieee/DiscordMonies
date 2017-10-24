94245d0389decb958d500a03c3e628956876547e
Aren Merzoian <aren.cllc@gmail.com>
2017-10-24 18:09:22 +0200

    Update Class Loader

--- classes/classloader.js
+++ classes/classloader.js
@@ -7,6 +7,12 @@ exports.BoardItems = {
   Go:require('./boarditems/go.js').Go,
   Station:require('./boarditems/station.js').Station,
   Utiity:require('./boarditems/utility.js').Utility,
+  Chance:require('./boarditems/chance.js').Chance,
+  Chest:require('./boarditems/chest.js').Chest,
+  Jail:require('./boarditems/jail.js').Jail,
+  Parking:require('./boarditems/parking.js').Parking,
+  Tax:require('./boarditems/tax.js').Tax,
+  ToJail:require('./boarditems/tojail.js').ToJail,
 };
 exports.Board = require('./structures/board.js').Board;
 exports.Game = require('./structures/game.js').Game;
